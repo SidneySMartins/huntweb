08f43780ba9198f49a8c1b494f6552f6067a2fc9
SidneySMartins <sidneysmsantos@gmail.com>
2020-11-26 17:58:47 -0300

    first commit

---
+++ src/components/pages/main/index.js
@@ -0,0 +1,10 @@
+import React, {component} from 'react'
+import api from '../../services/api'
+
+export default class Main extends component{
+    render(){
+        
+        return <h1>Hello World!</h1>
+
+    }
+}
